6bcf652116bb769d0c7e97f7041427047e4ba09f
arathore2023 rathore <arathore@altametrics.com>
2024-09-30 22:32:58 +0000

    fake

--- counter/vite.config.js
+++ counter/vite.config.js
@@ -1,7 +1,12 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 3000,      // Ensure this matches the port you expose
+  },
 })
